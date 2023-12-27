caa2cc9b5238564a9f070301f12f756df951d1af
icodesign <leimagnet@gmail.com>
2023-12-27 15:14:47 +0800

    Update dolphin.rb

--- Formula/dolphin.rb
+++ Formula/dolphin.rb
@@ -5,7 +5,7 @@ class Dolphin < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/therootlab/dolphin-hq/releases/download/0.1.16/dolphin-macos-x64.zip"
+      url "https://github.com/therootlab/dolphin-hq/releases/download/cli-0.1.16/dolphin-macos-x64.zip"
       sha256 "c54684519dcdffe84ad88855bf2eef1b323b8061da5ccc71ac183e15c53d5220"
 
       def install
@@ -13,7 +13,7 @@ class Dolphin < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/therootlab/dolphin-hq/releases/download/0.1.16/dolphin-macos-arm64.zip"
+      url "https://github.com/therootlab/dolphin-hq/releases/download/cli-0.1.16/dolphin-macos-arm64.zip"
       sha256 "e372487a4996930209fae558bc60739bbaa644349ecb95acce7373ebba107757"
 
       def install
